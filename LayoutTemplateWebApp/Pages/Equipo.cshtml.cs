b186e33667e7dbf705646613274489628761c64e
kpalaciosm02 <60985460+kpalaciosm02@users.noreply.github.com>
2023-10-11 03:04:26 -0600

    Pages created and basic navigation added.

---
+++ LayoutTemplateWebApp/Pages/Equipo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LayoutTemplateWebApp.Pages
+{
+    public class EquipoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
